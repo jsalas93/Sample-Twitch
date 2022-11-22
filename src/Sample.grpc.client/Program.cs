2b332d33ae309b24e9e624827785b367a8688a44
Josue Salas <josue.salas@defisolutions.com>
2022-11-22 09:23:20 -0600

    add grpc client

---
+++ src/Sample.grpc.client/Program.cs
@@ -0,0 +1,41 @@
+﻿using Grpc.Net.Client;
+using MassTransit;
+using Sample.grpc.client;
+
+Console.WriteLine("To test methods, press 1  to delete, 2 to get, 3 to post, 4 to patch or 5 to put");
+var val = Console.ReadLine();
+using var channel = GrpcChannel.ForAddress("http://localhost:5236");
+
+var orderClient = new Order.OrderClient(channel);
+
+var customerClient = new Customer.CustomerClient(channel);
+
+switch (val)
+{
+    case "1":
+        var replyDeleteCustomer = await customerClient.DeleteAsync(new CustomerRequest() { Id = NewId.NextGuid().ToString(), CustomerNumber = "12345" });
+        Console.WriteLine($"Reply Delete Customer {replyDeleteCustomer}");
+        break;
+    case "2":
+        var replayGetOrder = await orderClient.GetAsync(new OrderGetRequest() { Id = NewId.NextGuid().ToString() });
+        Console.WriteLine($"Reply Get Order {replayGetOrder}");
+        break;
+    case "3":
+        var replayPostOrder = await orderClient.PostAsync(new OrderPostRequest() { Id = NewId.NextGuid().ToString(), CustomerNumber = "12345", PaymentCardNumber = "12345", Notes = "Test" });
+        Console.WriteLine($"Reply Post Order {replayPostOrder}");
+        break;
+    case "4":
+        var replayPatchOrder = await orderClient.PatchAsync(new OrderPatchRequest() { Id = NewId.NextGuid().ToString() });
+        Console.WriteLine($"Reply Patch Order {replayPatchOrder}");
+        break;
+    case "5":
+        var replayPutOrder = await orderClient.PutAsync(new OrderPutRequest() { Id = NewId.NextGuid().ToString(), CustomerNumber = "12345" });
+        Console.WriteLine($"Reply Put Order {replayPutOrder}");
+        break;
+    default:
+        Console.WriteLine("Option invalid");
+        break;
+}
+
+Console.WriteLine("press any key to exit");
+Console.ReadLine();
--- src/Sample.grpc/Program.cs
+++ src/Sample.grpc/Program.cs
@@ -1,12 +1,47 @@
+using MassTransit;
 using Sample.grpc.Services;
+using Sample.Components.Consumers;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Sample.Contracts;
+using Microsoft.ApplicationInsights.DependencyCollector;
+using MassTransit.MongoDbIntegration.MessageData;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+bool _isRunningInContainer = bool.TryParse(Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER"), out var inContainer) && inContainer;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-
-// Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddHealthChecks();
+builder.Services.AddApplicationInsightsTelemetry();
+builder.Services.ConfigureTelemetryModule<DependencyTrackingTelemetryModule>((module, o) =>
+{
+    module.IncludeDiagnosticSourceActivities.Add("MassTransit");
+});
+builder.Services.TryAddSingleton(KebabCaseEndpointNameFormatter.Instance);
+builder.Services.AddMassTransit(mt =>
+{
+    mt.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(_isRunningInContainer ? "rabbitmq" : "localhost");
+
+        MessageDataDefaults.ExtraTimeToLive = TimeSpan.FromDays(1);
+        MessageDataDefaults.Threshold = 2000;
+        MessageDataDefaults.AlwaysWriteToRepository = false;
+        cfg.UseMessageData(new MongoDbMessageDataRepository(_isRunningInContainer ? "mongodb://mongo" : "mongodb://127.0.0.1", "attachments"));
+    });
+    mt.AddRequestClient<SubmitOrder>(new Uri($"queue:{KebabCaseEndpointNameFormatter.Instance.Consumer<SubmitOrderConsumer>()}"));
+    mt.AddRequestClient<CheckOrder>();
+});
+builder.Services.Configure<HealthCheckPublisherOptions>(options =>
+{
+    options.Delay = TimeSpan.FromSeconds(2);
+    options.Predicate = check => check.Tags.Contains("ready");
+});
+builder.Services.AddOpenApiDocument(cfg => cfg.PostProcess = d => d.Info.Title = "Sample API Site");
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -16,3 +51,27 @@ app.MapGrpcService<CustomerService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
+
+app.UseOpenApi(); // serve OpenAPI/Swagger documents
+app.UseSwaggerUi3(); // serve Swagger UI
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+
+    // The readiness check uses all registered checks with the 'ready' tag.
+    endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions
+    {
+        Predicate = check => check.Tags.Contains("ready"),
+    });
+
+    endpoints.MapHealthChecks("/health/live", new HealthCheckOptions
+    {
+        // Exclude all checks and return a 200-Ok.
+        Predicate = _ => false
+    });
+});
